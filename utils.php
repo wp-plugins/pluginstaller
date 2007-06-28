7237298dae952fb4e5db550cd50400fc7dde62ad
hschaefer <hschaefer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-06-28 17:33:08 +0000

    git-svn-id: https://plugins.svn.wordpress.org/pluginstaller/branches/0.2@14648 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- utils.php
+++ utils.php
@@ -37,7 +37,7 @@ function u_get_contents ( $filename ) {
 
 // Check for PlugInstaller updates:
 function pi_check_for_update() {
-  $current_version = '0.1.7';
+  $current_version = '0.1.9';
   $fp = uopen('http://henning.imaginemore.de/pi-version.txt','r');
   $available_version = fgets($fp);
   uclose($fp);
